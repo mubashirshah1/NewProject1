e046c397988c865227740eb31fee4584fcf7f554
mubashirshah1 <64752619+mubashirshah1@users.noreply.github.com>
2021-05-30 21:27:37 +0500

    My First Commit

---
+++ Lab4Main.java
@@ -0,0 +1,17 @@
+public class Lab4Main
+{
+	public static void main(String[] args)
+	{
+		task1 thread = new task1();
+		try
+		{
+			thread.sleep(1000);
+			thread.start();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
